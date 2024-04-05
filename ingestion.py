ec463bfdc7c231c2d997ae59bf255ca615a14221
Katya Govorkova <katyag@mit.edu>
2024-04-05 07:41:57 -0700

    clean up

--- ingestion.py
+++ ingestion.py
@@ -1,17 +1,9 @@
-#------------------------------------------
-# Imports
-#------------------------------------------
 import sys
 import os
 import numpy as np
-import pandas as pd 
+import pandas as pd
 import time
 
-
-
-#------------------------------------------
-# Directories
-#------------------------------------------
 # Input data directory to read training data from
 input_dir = 'input_data/'
 
@@ -20,29 +12,7 @@ output_dir = 'output/'
 
 sys.path.append(output_dir)
 
-#------------------------------------------
-# Read Train Data
-#------------------------------------------
-def get_training_data():
 
-    # set train data and solution file
-    train_data_file = os.path.join(input_dir, 'train.csv')
-    train_solution_file = os.path.join(input_dir, 'train.labels')
-    
-    # Read Train data
-    X_train = pd.read_csv(train_data_file)
-
-    # Read Train solution
-    f = open(train_solution_file, "r")
-    y_train = f.read().splitlines()
-    y_train = np.array(y_train,dtype=float)
-
-
-    return X_train, y_train
-
-#------------------------------------------
-# Read Test Data
-#------------------------------------------
 def get_prediction_data():
 
     # set test data and solution file
@@ -53,9 +23,6 @@ def get_prediction_data():
 
     return X_test
 
-#------------------------------------------
-# Save Predictions
-#------------------------------------------
 def save_prediction(prediction_prob):
 
     prediction_file = os.path.join(output_dir, 'test.predictions')
@@ -70,70 +37,42 @@ def save_prediction(prediction_prob):
             else:
                 f.write(str_label)
 
-    
+
 def print_pretty(text):
     print("-------------------")
     print("#---",text)
     print("-------------------")
 
-#------------------------------------------
-# Run the pipeline 
-# > Load 
-# > Train 
-# > Predict 
-# > Save
-#------------------------------------------
-def main():
 
-    #------------------------------------------
-    # Start Timer
-    #------------------------------------------
+def main():
+    """
+     Run the pipeline
+     > Load
+     > Train
+     > Predict
+     > Save
+    """
+
     start = time.time()
 
-
-    #------------------------------------------
-    # Import Model
-    #------------------------------------------
     from model import Model
-   
-    #------------------------------------------
-    # Read Data
-    #------------------------------------------
+
     print_pretty('Reading Data')
-    X_train, y_train = get_training_data()
     X_test = get_prediction_data()
 
-
-    #------------------------------------------
-    # Load Model
-    #------------------------------------------
     print_pretty('Starting Learning')
     m = Model()
+    # the model should be loaded here !!
 
-    #------------------------------------------
-    # Train Model
-    #------------------------------------------
-    print_pretty('Training Model')
-    m.fit(X_train, y_train)
-
-    #------------------------------------------
-    # Make Predictions
-    #------------------------------------------
     print_pretty('Making Prediction')
     prediction_prob = m.predict_score(X_test)
 
-    #------------------------------------------
-    # Save  Predictions
-    #------------------------------------------
     print_pretty('Saving Prediction')
     save_prediction(prediction_prob)
 
-
-    #------------------------------------------
-    # Show Ingestion Time
-    #------------------------------------------
     duration = time.time() - start
     print_pretty(f'Total duration: {duration}')
+
 
 if __name__ == '__main__':
     main()
--- scoring.py
+++ scoring.py
@@ -1,36 +1,15 @@
-#------------------------------------------
-# Imports
-#------------------------------------------
 import sys
 import os
 import json
 import numpy as np
 
-
-
-#------------------------------------------
-# Import Metric
-#------------------------------------------
-# from metric import auc_metric
-
-
-#------------------------------------------
-# constants
-#------------------------------------------
-data_name = "fair_universe"
-
-#------------------------------------------
-# directories
-#------------------------------------------
 # Directory read predictions and solutions from
-input_dir = 'input_data/' 
+input_dir = 'input_data/'
 
 # Directory to output computed score into
 output_dir = 'output/'
 
-#------------------------------------------
-# Read Predictions
-#------------------------------------------
+
 def read_prediction():
     prediction_file = os.path.join(output_dir,'test.predictions')
 
@@ -42,15 +21,13 @@ def read_prediction():
 
 
     f = open(prediction_file, "r")
-    
+
     predicted_scores = f.read().splitlines()
     predicted_scores = np.array(predicted_scores,dtype=float)
-    
+
     return predicted_scores
 
-#------------------------------------------
-# Read Solutions
-#------------------------------------------
+
 def read_solution():
 
     solution_file = os.path.join(input_dir, 'test.labels')
@@ -67,6 +44,7 @@ def read_solution():
 
     return test_labels
 
+
 def save_score(score):
     score_file = os.path.join(output_dir, 'scores.json')
 
@@ -77,6 +55,7 @@ def save_score(score):
         f_score.write(json.dumps(scores))
         f_score.close()
 
+
 def print_pretty(text):
     print("-------------------")
     print("#---",text)
@@ -86,28 +65,19 @@ def print_pretty(text):
     
 def main():
 
-
-    #------------------------------------------
     # Read prediction and solution
-    #------------------------------------------
     print_pretty('Reading prediction')
     prediction = read_prediction()
     solution = read_solution()
 
-
-    #------------------------------------------
     # Compute Score
-    #------------------------------------------
     print_pretty('Computing score')
     score = (prediction == solution).sum() / len(prediction)
     print("Accuracy: ", score)
 
-    #------------------------------------------
     # Write Score
-    #------------------------------------------
     print_pretty('Saving prediction')
     save_score(score)
-
 
 
 if __name__ == '__main__':
